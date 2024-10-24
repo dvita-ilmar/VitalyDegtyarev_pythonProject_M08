67adb9dfb45987332bfd7adb87dbe4558ccb7ebc
dvita-ilmar <dvita@mail.ru>
2024-10-24 14:38:36 +0500

    module_8_1.py is completed

---
+++ module_8_1.py
@@ -0,0 +1,20 @@
+"""
+Дегтярев Виталий (группа 22/08)
+Домашнее задание №8.1
+Домашнее задание по теме "Try и Except"
+"""
+
+
+def add_everything_up(a, b):
+    try:
+        return a + b
+    except TypeError:
+        return str(a) + str(b)
+
+
+# Запуск
+if __name__ == '__main__':
+
+    print(add_everything_up(123.456, 'строка')) # 123.456строка
+    print(add_everything_up('яблоко', 4215)) # яблоко4215
+    print(add_everything_up(123.456, 7)) #130.45600000000002
